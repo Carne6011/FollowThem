374b872789f36c1f827f39474d6035bb0c2a1030
Carne6011 <56942406+Carne6011@users.noreply.github.com>
2021-10-25 20:46:43 -0400

    First update, partially finished game, Other school very difficult at the time, so will try to do a bit every day, but unsure if I will have time to finish completely, If I can get the movement working I will be happy.

--- app/src/main/java/com/example/hacktoberfest_2021/MainActivity.java
+++ app/src/main/java/com/example/hacktoberfest_2021/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.hacktoberfest_2021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ActionBar;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
+import android.view.View;
+
+import android.content.Intent;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.lang.reflect.Field;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    int x_cord = 606; //606
+    int y_cord = 65;//65
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_true);
     }
-}
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+        final ImageView imageView = (ImageView)findViewById(R.id.MinerHat);
+        imageView.setX(x_cord);
+        imageView.setY(y_cord);
+        imageView.invalidate();
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }}

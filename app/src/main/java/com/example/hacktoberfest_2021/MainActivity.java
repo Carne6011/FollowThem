47bc9bf980a0cb12438ea8d478f3906eb0201ddc
Carne6011 <56942406+Carne6011@users.noreply.github.com>
2021-10-30 21:57:07 -0400

    Graphics are now looking aliot better, and it compiles with no errors, but the movement is still lacking.

--- app/src/main/java/com/example/hacktoberfest_2021/MainActivity.java
+++ app/src/main/java/com/example/hacktoberfest_2021/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.hacktoberfest_2021;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.ActionBar;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +17,7 @@ import android.content.Intent;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Random;
 
@@ -31,13 +35,23 @@ public class MainActivity extends AppCompatActivity {
     int[][] boulderlocation = new int[5][2];
     int randomx;
     int randomy;
-
+    ///////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.battle_setup);
+
+                setContentView(R.layout.battle_setup);
+        onWindowFocusChanged(true);
         setupmap();
         startlocations();
+        BoulderSetUp();
+        UpdateGrid();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onPause() {
     }
 
     @Override
@@ -46,13 +60,11 @@ public class MainActivity extends AppCompatActivity {
         if (hasFocus) {
             hideSystemUI();
         }
-        final ImageView imageView = (ImageView)findViewById(R.id.MinerHat);
-        imageView.setX(x_s);
-        imageView.setY(y_s);
-        imageView.invalidate();
+
+
     }
 
-    private void hideSystemUI(){
+    private void hideSystemUI() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE
@@ -62,13 +74,10 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
-    private void showSystemUI(){
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-    }
+
+
+    //////////////
+
 
     public void setupmap()//sets up the playing field
     {
